31f6d94d805a5f9a1db9b10f6816a8daf6d56163
Abhay Raj Singh <abhayraja4@gmail.com>
2022-04-03 20:19:00 -0400

    profile update form & navbar css

--- myApp1/forms.py
+++ myApp1/forms.py
@@ -17,3 +17,11 @@ class CustomerForm(ModelForm):
         model = Customer
         fields = '__all__'
         exclude = ['user']
+
+    def __init__(self, *args, **kwargs):
+        common_field_css = {'class': 'p-2 my-3 text-lg font-medium mb-3 text-black'}
+        super(CustomerForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update(common_field_css)
+        self.fields['phone'].widget.attrs.update(common_field_css)
+        self.fields['email'].widget.attrs.update(common_field_css)
+        self.fields['profile_pic'].widget.attrs.update(common_field_css)
--- myApp1/models.py
+++ myApp1/models.py
@@ -55,13 +55,15 @@ class OrderItem(models.Model):
     def total_price(self):
         return self.item.price * self.no_of_items
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
-    #date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    # date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
